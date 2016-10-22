d6431a7d448179693deffc0e3a0a3d9807f49e87
Yehor Dzhurynskyi <y.a.dzhurynskyi@gmail.com>
2016-10-22 19:36:00 +0300

    working on slider autoscroller

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(function() {
+$(document).ready(function() {
 	$('.jcarousel')
 		.on('jcarousel:create jcarousel:reload', function() {
 			var element = $(this);
@@ -8,14 +8,19 @@ $(function() {
 		.jcarousel({
 			vertical: true,
 			animation: 'slow',
-			wrap: 'circular'
+			wrap: 'circular',
+		})
+		.jcarouselAutoscroll({
+			interval: 3000,
+			target: '+=1',
+			autostart: true
 		});
 
 	$('.jcarousel-prev').jcarouselControl({
-		target: '-=1'
+			target: '-=1',
 	});
 
 	$('.jcarousel-next').jcarouselControl({
-		target: '+=1'
+			target: '+=1'
 	});
 });
